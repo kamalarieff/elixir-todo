5f5b6e1847ebb7b10cbf42ec0bae4cf1310adae8
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-07 19:28:54 +0800

    render a different layout

--- lib/todo_web/controllers/page_controller.ex
+++ lib/todo_web/controllers/page_controller.ex
@@ -2,6 +2,8 @@ defmodule TodoWeb.PageController do
   use TodoWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_layout("admin.html")
+    |> render("index.html")
   end
 end
