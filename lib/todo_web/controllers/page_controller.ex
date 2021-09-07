415e3643642e71f862e7218b736aa16bb004bc61
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-07 20:27:22 +0800

    redirect using router helpers

--- lib/todo_web/controllers/page_controller.ex
+++ lib/todo_web/controllers/page_controller.ex
@@ -2,8 +2,6 @@ defmodule TodoWeb.PageController do
   use TodoWeb, :controller
 
   def index(conn, _params) do
-    conn
-    |> put_resp_content_type("text/plain")
-    |> send_resp(201, "")
+    redirect(conn, to: Routes.items_path(conn, :index))
   end
 end
