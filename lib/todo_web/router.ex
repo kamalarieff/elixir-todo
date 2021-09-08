d31b9f9abd8a3511fa751bed90121d79f3594d87
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-08 23:12:21 +0800

    authenticating users

--- lib/todo_web/router.ex
+++ lib/todo_web/router.ex
@@ -22,8 +22,21 @@ defmodule TodoWeb.Router do
     patch "/items/:id/complete", ItemsController, :complete
     delete "/items/:id", ItemsController, :delete
     resources "/users", UserController
+    resources "/sessions", SessionController, only: [:new, :create, :delete], singleton: true
   end
 
+
+  defp authenticate_user(conn, _) do
+    case get_session(conn, :user_id) do
+      nil ->
+        conn
+        |> Phoenix.Controller.put_flash(:error, "Login required")
+        |> Phoenix.Controller.redirect(to: "/")
+        |> halt()
+      user_id ->
+        assign(conn, :current_user, Todo.Accounts.get_user!(user_id))
+    end
+  end
   # Other scopes may use custom stacks.
   # scope "/api", TodoWeb do
   #   pipe_through :api
