0afafe5a2e1a671ffdafa9fef4b62265f23f0e04
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-10 00:01:21 +0800

    add commented code for disabling logging

--- config/config.exs
+++ config/config.exs
@@ -26,6 +26,9 @@ config :logger, :console,
 # Use Jason for JSON parsing in Phoenix
 config :phoenix, :json_library, Jason
 
+# You can disable logging via this line
+# config :phoenix, :logger, false
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
 import_config "#{Mix.env()}.exs"
