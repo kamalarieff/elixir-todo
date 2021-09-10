f1d3936ebf73f598d1612fd4ac092db6804474f1
Kamal Arieff <kamal.arieff@mudah.my>
2021-09-11 00:03:13 +0800

    initial testing for phoenix

--- test/todo_web/views/error_view_test.exs
+++ test/todo_web/views/error_view_test.exs
@@ -1,13 +1,17 @@
 defmodule TodoWeb.ErrorViewTest do
   use TodoWeb.ConnCase, async: true
 
+  @moduletag :error_view_case
+
   # Bring render/3 and render_to_string/3 for testing custom views
   import Phoenix.View
 
+  @tag individual_test: "yup"
   test "renders 404.html" do
     assert render_to_string(TodoWeb.ErrorView, "404.html", []) == "Not Found"
   end
 
+  @tag individual_test: "nope"
   test "renders 500.html" do
     assert render_to_string(TodoWeb.ErrorView, "500.html", []) == "Internal Server Error"
   end
